a58ebb233bafb405558c05fce82025f3706e612b
phanthethanh0209 <phanthethanh0209@gmail.com>
2024-10-21 12:00:20 +0700

    Update database dung fluent API va viet, xu ly API cho bang Flight

---
+++ Tuan 2 - Bao cao 1/TheThanh_WebAPI_Flight/Data/User.cs
@@ -0,0 +1,15 @@
+﻿namespace TheThanh_WebAPI_Flight.Data
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+
+        // relationship
+        public ICollection<DocumentType> DocumentTypes { get; set; } = new List<DocumentType>();
+
+    }
+}
---
+++ Tuan 2 - Bao cao 1/TheThanh_WebAPI_Flight/Migrations/20241021030503_DBInit.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheThanh_WebAPI_Flight.Migrations
+{
+    public partial class DBInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flight",
+                columns: table => new
+                {
+                    FlightID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FlightNo = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DepartureDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PointLoad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PointUnload = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalDoc = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flight", x => x.FlightID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocumentType",
+                columns: table => new
+                {
+                    TypeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentType", x => x.TypeID);
+                    table.ForeignKey(
+                        name: "FK_DocumentType_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Document",
+                columns: table => new
+                {
+                    DocID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DocName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    File = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TypeID = table.Column<int>(type: "int", nullable: false),
+                    FlightID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Document", x => x.DocID);
+                    table.ForeignKey(
+                        name: "FK_Document_DocumentType_TypeID",
+                        column: x => x.TypeID,
+                        principalTable: "DocumentType",
+                        principalColumn: "TypeID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Document_Flight_FlightID",
+                        column: x => x.FlightID,
+                        principalTable: "Flight",
+                        principalColumn: "FlightID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Document_FlightID",
+                table: "Document",
+                column: "FlightID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Document_TypeID",
+                table: "Document",
+                column: "TypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentType_UserID",
+                table: "DocumentType",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flight_FlightNo",
+                table: "Flight",
+                column: "FlightNo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "User",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Document");
+
+            migrationBuilder.DropTable(
+                name: "DocumentType");
+
+            migrationBuilder.DropTable(
+                name: "Flight");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
