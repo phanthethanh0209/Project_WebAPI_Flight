9d6bc3ecf6524b675e41ee4d61d70878e59ab4ac
phanthethanh0209 <phanthethanh0209@gmail.com>
2024-10-28 12:45:52 +0700

    Hoan thien up va download file

---
+++ Tuan 3 - Bao cao 1/TheThanh_WebAPI_Flight/Controllers/FlightController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheThanh_WebAPI_Flight.DTO;
+using TheThanh_WebAPI_Flight.Services;
+
+namespace TheThanh_WebAPI_Flight.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        private readonly IFlightService _flightService;
+
+        public FlightController(IFlightService flightService)
+        {
+            _flightService = flightService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllFLight(int page = 1)
+        {
+            return Ok(await _flightService.GetAllFLight(page));
+        }
+
+        [HttpGet("{FlightNo}")]
+        public async Task<IActionResult> GetByFlightNo(string flightNo)
+        {
+            FlightDTO flight = await _flightService.GetFLight(flightNo);
+            if (flight == null) return BadRequest("Not found");
+
+            return Ok(flight);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFlight(CreateFlightDTO createDto)
+        {
+            (bool Success, string ErrorMessage) result = await _flightService.CreateFlight(createDto);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+            return Ok(await _flightService.GetAllFLight());
+        }
+
+        [HttpPut("{FlightID}")]
+        public async Task<IActionResult> UpdateFlight(int flightID, CreateFlightDTO updateDto)
+        {
+            (bool Success, string ErrorMessage) result = await _flightService.UpdateFlight(flightID, updateDto);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+
+            return Ok(await _flightService.GetAllFLight());
+        }
+
+        [HttpDelete("{FlightID}")]
+        public async Task<IActionResult> DeleteFlight(int flightID)
+        {
+            (bool Success, string ErrorMessage) result = await _flightService.DeleteFlight(flightID);
+            if (!result.Success)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+            return Ok(await _flightService.GetAllFLight());
+        }
+    }
+}
---
+++ Tuan 3 - Bao cao 1/TheThanh_WebAPI_Flight/Mapper/MappingDocument.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TheThanh_WebAPI_Flight.Data;
+using TheThanh_WebAPI_Flight.DTO;
+
+namespace TheThanh_WebAPI_Flight.Mapper
+{
+    public class MappingDocument : Profile
+    {
+        public MappingDocument()
+        {
+            CreateMap<CreateDocumentDTO, Document>();
+            CreateMap<Document, DocumentDTO>()
+                .ForMember(dest => dest.FlightNo, opt => opt.MapFrom(src => src.Flights.FlightNo))
+                .ForMember(dest => dest.TypeName, opt => opt.MapFrom(src => src.DocumentTypes.TypeName));
+        }
+    }
+}
