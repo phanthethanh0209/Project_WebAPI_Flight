4be00518da8caf970d20b978d294406c94e09c22
phanthethanh0209 <phanthethanh0209@gmail.com>
2024-11-04 12:12:31 +0700

    Hoan thien phan quyen user tren tung tai lieu va loai tai lieu

---
+++ Tuan 4 - Bao cao 1/TheThanh_WebAPI_Flight/Controllers/DocumentController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheThanh_WebAPI_Flight.DTO;
+using TheThanh_WebAPI_Flight.Services;
+using static TheThanh_WebAPI_Flight.Authorization.CustomAuthorizationAttribute;
+
+namespace TheThanh_WebAPI_Flight.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        private readonly IDocumentService _docService;
+
+        public DocumentController(IDocumentService docService)
+        {
+            _docService = docService;
+        }
+
+        [CustomAuthorize(new[] { "Read Only", "Read and modify", "Full permission" })]
+        [HttpGet]
+        public async Task<IActionResult> GetAllDoc(int page = 1)
+        {
+            return Ok(await _docService.GetAllDocument(page));
+        }
+
+        [CustomAuthorize(new[] { "Read Only", "Read and modify", "Full permission" })]
+        [HttpGet("{DocID}")]
+        public async Task<IActionResult> GetByDocID(int docID)
+        {
+            DocumentDTO doc = await _docService.GetDocByID(docID);
+            if (doc == null) return BadRequest("Not found");
+
+            return Ok(doc);
+        }
+
+        [CustomAuthorize(new[] { "Full permission" })]
+        [HttpPost]
+        public async Task<IActionResult> CreateDocument([FromForm] CreateDocumentDTO createDto)
+        {
+            (bool Success, string ErrorMessage) result = await _docService.CreateDocument(createDto);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+            return Ok(await _docService.GetAllDocument());
+        }
+
+        [CustomAuthorize(new[] { "Read Only", "Read and modify", "Full permission" })]
+        [HttpGet]
+        [Route("Download/{DocId}")]
+        public async Task<IActionResult> Download(int DocID)
+        {
+            // Gọi phương thức DownloadFile từ service để lấy dữ liệu file
+            (byte[] fileData, string contentType, string originalFileName) = await _docService.DownloadFile(DocID);
+
+            // Trả về file dưới dạng response HTTP
+            return File(fileData, contentType, originalFileName);
+        }
+
+        //[HttpPut("{DocId}")]
+        //public async Task<IActionResult> UpdateUser(int flightID, CreateFlightDTO updateDto)
+        //{
+        //    (bool Success, string ErrorMessage) result = await _flightService.UpdateFlight(flightID, updateDto);
+
+        //    if (!result.Success)
+        //    {
+        //        return BadRequest(result.ErrorMessage);
+        //    }
+
+        //    return Ok(await _flightService.GetAllFLight());
+        //}
+
+        [CustomAuthorize(new[] { "Full permission" })]
+        [HttpDelete("{DocID}")]
+        public async Task<IActionResult> DeleteDocument(int docID)
+        {
+            (bool Success, string ErrorMessage) result = await _docService.DeleteDocument(docID);
+            if (!result.Success)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+            return Ok(await _docService.GetAllDocument());
+        }
+    }
+}
---
+++ Tuan 4 - Bao cao 1/TheThanh_WebAPI_Flight/Services/DocumentService.cs
@@ -0,0 +1,152 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.StaticFiles;
+using System.Security.Claims;
+using TheThanh_WebAPI_Flight.Data;
+using TheThanh_WebAPI_Flight.DTO;
+using TheThanh_WebAPI_Flight.Repository;
+
+namespace TheThanh_WebAPI_Flight.Services
+{
+    public interface IDocumentService
+    {
+        Task<IEnumerable<DocumentDTO>> GetAllDocument(int page = 1);
+        Task<DocumentDTO> GetDocByID(int docID);
+        Task<(bool Success, string ErrorMessage)> CreateDocument(CreateDocumentDTO createDTO);
+        Task<(byte[] FileData, string ContentType, string FileName)> DownloadFile(int docID);
+
+        //Task<(bool Success, string ErrorMessage)> UpdateFlight(int id, CreateFlightDTO updateFlightDTO);
+        Task<(bool Success, string ErrorMessage)> DeleteDocument(int id);
+    }
+
+    public class DocumentService : IDocumentService
+    {
+        private readonly IRepositoryWrapper _repository;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DocumentService(IRepositoryWrapper repository, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<(bool Success, string ErrorMessage)> CreateDocument(CreateDocumentDTO createDTO)
+        {
+            Document newDoc = _mapper.Map<Document>(createDTO);
+            // Lấy HttpContext và kiểm tra nếu người dùng đã đăng nhập
+            HttpContext? httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext?.User?.Identity != null && httpContext.User.Identity.IsAuthenticated)
+            {
+                Claim? userIdClaim = httpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+                if (userIdClaim != null && int.TryParse(userIdClaim.Value, out int userId))
+                {
+                    newDoc.UserID = userId;
+                }
+                else
+                {
+                    return (false, "Cannot retrieve the user ID from claims.");
+                }
+            }
+            else
+            {
+                return (false, "User is not authenticated.");
+            }
+
+            Flight flight = await _repository.Flight.GetByIdAsync(u => u.FlightID == createDTO.FlightID);
+            if (flight == null)
+            {
+                return (false, "flight has not been created yet");
+            }
+
+            string fileResult = await WriteFile(createDTO.DocName, flight.FlightNo);
+            if (string.IsNullOrEmpty(fileResult))
+            {
+                return (false, "Error saving file.");
+            }
+
+            newDoc.DocName = fileResult;
+
+            await _repository.Document.CreateAsync(newDoc);
+            return (true, null);
+        }
+        private async Task<string> WriteFile(IFormFile file, string flightID)
+        {
+            string fileName = file.FileName;
+            try
+            {
+                //Đường dẫn của thư mục 'Upload/Files' được tạo từ thư mục hiện tại
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files", flightID);
+
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath); // tạo thư mục
+                }
+
+                string exactpath = Path.Combine(filePath, fileName); // lấy đg dẫn đến file được lưu trữ
+                //ghi file vào đường dẫn đã xác định
+                using (FileStream stream = new(exactpath, FileMode.Create))
+                {
+                    //Sao chép nội dung file từ đối tượng IFormFile vào luồng FileStream
+                    await file.CopyToAsync(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return fileName;
+        }
+
+        public async Task<(byte[] FileData, string ContentType, string FileName)> DownloadFile(int docID)
+        {
+            Document doc = await _repository.Document.GetByIdAsync(u => u.DocID == docID);
+
+            // Lấy chuyến bay tương ứng với tài liệu này
+            Flight flight = await _repository.Flight.GetByIdAsync(f => f.FlightID == doc.FlightID);
+
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files", flight.FlightNo, doc.DocName);
+
+            // Lấy kiểu nội dung file (dùng để ánh xạ các phần mở rộng file (như .txt, .jpg, .pdf) sang kiểu nội dung phù hợp (như text/plain, image/jpeg, application/pdf))
+            FileExtensionContentTypeProvider provider = new();
+            if (!provider.TryGetContentType(filePath, out string? contentType)) // lấy kiểu file dựa trên phần mở rộng
+            {
+                contentType = "application/octet-stream";
+            }
+
+            // Đọc file thành mảng byte để hệ thống có thể xử lý và truyền tải
+            byte[] bytes = await File.ReadAllBytesAsync(filePath);
+
+            // Trả về file data, kiểu nội dung và tên file
+            return (bytes, contentType, Path.GetFileName(filePath));
+        }
+
+        public async Task<IEnumerable<DocumentDTO>> GetAllDocument(int pageNumber)
+        {
+            IEnumerable<Document> documents = await _repository.Document.GetAllWithPaginationAsync(null, pageNumber, 3, doc => doc.Flights, doc => doc.DocumentTypes, u => u.Users);
+
+            return _mapper.Map<IEnumerable<DocumentDTO>>(documents);
+        }
+
+        public async Task<(bool Success, string ErrorMessage)> DeleteDocument(int id)
+        {
+            Document document = await _repository.Document.GetByIdAsync(u => u.DocID == id);
+            if (document == null)
+                return (false, "Document not found.");
+
+            await _repository.Document.DeleteAsync(document);
+            return (true, null);
+        }
+
+        public async Task<DocumentDTO> GetDocByID(int docID)
+        {
+            Document doc = await _repository.Document.GetByIdAsync(x => x.DocID == docID, doc => doc.Flights, doc => doc.DocumentTypes, u => u.Users);
+            if (doc == null)
+            {
+                return null;
+            }
+            return _mapper.Map<DocumentDTO>(doc);
+        }
+    }
+
+}
